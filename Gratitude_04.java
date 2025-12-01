a45eb62f2f932219c7d19ca029c04bd7044f7283
AisyatulHuriyah <aisyatulhuriyah08@gmail.com>
2025-12-01 21:32:48 +0700

    Experiment 2, original code

--- Gratitude_04.java
+++ Gratitude_04.java
@@ -1,6 +1,15 @@
-public class Gratitude_04{
+public class Gratitude_04 {
+    public static void sayThankyou() {
+        System.out.println("Thank you for being the best teacher in the world.\n" +
+                           "You inspired in me a love for learning and made me feel like I could ask you anything.");
+    }
+    public static void sayAdditionalGreetings(String greeting) {
+        System.out.println(greeting);
+    }
     public static void main(String[] args) {
-            System.out.println("Thank you for being the best teacher in the world.\n" +
-                               "You inspired in me a love for learning and made me feel like I could ask you anything.");
+        // Calling the function
+        sayThankyou();
+        String expression = "Thankyou...wish you all the best!!";
+        sayAdditionalGreetings(expression);
     }
 }
