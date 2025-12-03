db495c1324f0ad6d4bee38ec42d24b1d68eb69fc
AisyatulHuriyah <aisyatulhuriyah08@gmail.com>
2025-12-03 22:43:18 +0700

    answer no 3

--- ExpressingGratitude_04.java
+++ ExpressingGratitude_04.java
@@ -10,15 +10,18 @@ public class ExpressingGratitude_04 {
         return recipientName;
     }
 
+    public static void sayAdditionalGreetings(String additionalGreeting) {
+        System.out.println(additionalGreeting);
+    }
+
     public static void sayThankyou() {
-        // Calling getGreetingRecipient() inside sayThankyou()
         String name = getGreetingRecipient(); 
         System.out.println("Thank you " + name + " for being the best teacher in the world.\n" +
                            "You inspired in me a love for learning and made me feel like I could ask you anything.");
-    }
+        sayAdditionalGreetings("Keep up the great work!");
+                        }
 
     public static void main(String[] args) {
-        // Calling sayThankyou()
         sayThankyou();
     }
 }
