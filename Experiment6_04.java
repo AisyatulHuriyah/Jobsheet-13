0fcd2b4a1f1a1d6251433a806fd56d9c57e36f11
AisyatulHuriyah <aisyatulhuriyah08@gmail.com>
2025-12-04 00:33:43 +0700

    code 2

--- Experiment6_04.java
+++ Experiment6_04.java
@@ -1,5 +1,13 @@
 import java.util.Scanner;
 public class Experiment6_04 {
+    static int calculateArea (int 1, int w) {
+        int area = 1*w;
+        return area;
+    }
+    static int calculateVolume (int 1, int w, int h) {
+        int vol = calculateArea(1, w)*h;
+        return vol ;  
+    }
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int p, l, t, L, vol;
@@ -11,9 +19,9 @@ public class Experiment6_04 {
         System.out.print("Input height =");
         t = input.nextInt();
 
-        L = p * l;
+        L = calculateArea(p, l);
         System.out.println("Area of the rectangle = " + L);
-        vol = p * l * t;
+        vol = calculateVolume(p, l, t);
         System.out.println("Volume of the rectangle = " + vol);
         
         input.close();
