d1b176196ebe16062949ae9c54b646568f6d41bb
AisyatulHuriyah <aisyatulhuriyah08@gmail.com>
2025-12-06 02:52:57 +0700

    Answer no 3

--- WeeklyGrade_04.java
+++ WeeklyGrade_04.java
@@ -1,10 +1,15 @@
 import java.util.Scanner;
 public class WeeklyGrade_04 {
 
-    static String[] students = {"Sari", "Rina", "Yani", "Dwi", "Lusi"};
-    static String[] weeks = {"Week 1", "Week 2", "Week 3", "Week 4", "Week 5", "Week 6", "Week 7"};
+    public static String[] generateNames(int count, String prefix) {
+        String[] names = new String[count];
+        for (int i = 0; i < count; i++) {
+            names[i] = prefix + " " + (i + 1);
+        }
+        return names;
+    }
 
-    static void inputGrades(int[][] grades, Scanner sc) {
+    static void inputGrades(int[][] grades, String[] students, String[] weeks, Scanner sc) {
         for (int i = 0; i < grades.length; i++) {
             System.out.println("\nInput grades for " + students[i] + ":");
             for (int j = 0; j < grades[i].length; j++) {
@@ -14,7 +19,7 @@ public class WeeklyGrade_04 {
         }
     }
 
-    static void display(int [][] grades) {
+    static void display(int [][] grades, String[] students, String[] weeks) {
         System.out.printf("%-10s", "Name");
         for (int i = 0; i < weeks.length; i++) {
             System.out.printf("%-8s", weeks[i]);
@@ -30,7 +35,7 @@ public class WeeklyGrade_04 {
         }
     }
 
-    static void findWeekHighest(int[][] grades) {
+    static void findWeekHighest(int[][] grades, String[] weeks) {
         int maxWeek = 0;
         int maxTotal = -1;
 
@@ -48,7 +53,7 @@ public class WeeklyGrade_04 {
         System.out.println("Highest grade total is in " + weeks[maxWeek] + " with total = " + maxTotal);
     }
 
-    static void findTopStudent(int[][] grades) {
+    static void findTopStudent(int[][] grades, String[] students, String[] weeks) {
         int bestStudent = 0;
         int bestTotal = -1; 
 
@@ -73,22 +78,29 @@ public class WeeklyGrade_04 {
     }
 
     public static void main(String[] args) {
-        int[][] grades = {
-            // W1, W2, W3, W4, W5, W6, W7
-            {20, 19, 25, 20, 10, 0, 10}, // Sari
-            {30, 30, 40, 10, 15, 20, 25}, // Rina
-            {5, 0, 20, 25, 10, 5, 45},   // Yani
-            {50, 0, 7, 8, 0, 30, 60},    // Dwi
-            {15, 10, 16, 15, 10, 10, 5}  // Lusi
-        };  
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input the number of students: ");
+        int numStudents = sc.nextInt();
+
+        System.out.print("Input the number of weeks: ");
+        int numWeeks = sc.nextInt();
+        
+        int[][] grades = new int[numStudents][numWeeks];
+        String[] students = generateNames(numStudents, "Student");
+        String[] weeks = generateNames(numWeeks, "Week");
+
+        System.out.println("\n--- Input Grades ---");
+        inputGrades(grades, students, weeks, sc);
         
         System.out.println("\n=== All Students Grades ===");
-        display(grades);
+        display(grades, students, weeks);
 
         System.out.println("\n=== Week with Highest Total Grade ===");
-        findWeekHighest(grades);
+        findWeekHighest(grades, weeks);
 
         System.out.println("\n=== Student with Highest Total Grade ===");
-        findTopStudent(grades);
+        findTopStudent(grades, students, weeks);
+
+        sc.close();
     }
 }
